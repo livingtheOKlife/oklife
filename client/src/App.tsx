68a8f2cb32225d03c0979f0f8c9495b51939ff2f
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-06-15 20:40:06 +0100

    build[client]: add initial frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="App">
+      <header id="header-container">HeaderContainer - OKlife</header>
+      <main id="main-container">MainContainer</main>
+      <footer id="footer-container">
+        <span>
+          <h1>livingthe<em>OK</em>life</h1> &copy; 2025
+        </span>
+        <span>
+          All rights reserved.
+        </span>
+      </footer>
+    </div>
+  )
+}
+
+export default App
