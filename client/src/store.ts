5e046f24731166df29d1cd715f96f469a9dee48b
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-06-15 21:00:17 +0100

    build[client]: add store

---
+++ client/src/store.ts
@@ -0,0 +1,25 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import authReducer from './api/authSlice'
+
+import { apiSlice } from './api/apiSlice'
+
+/**------------------------------ create store
+ *
+ * @name store
+ * @requires configureStore @reduxjs/toolkit
+ * @requires authReducer authSlice
+ * @requires apiSlice
+ * @description handles api reducers and middleware
+ *
+ * --------------- */
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware)
+})
+
+export default store
