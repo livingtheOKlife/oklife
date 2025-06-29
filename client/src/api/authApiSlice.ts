6c5a248f074589f3418b40b545dabfee1ae7f9d9
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-06-29 19:35:32 +0100

    feat[client]: add authentication mutation - create account mutation

--- client/src/api/authApiSlice.ts
+++ client/src/api/authApiSlice.ts
@@ -5,9 +5,22 @@ import { apiSlice } from './apiSlice'
  * @name authApiSlice
  * @requires apiSlice
  * @description authentication mutation builder
+ * @method POST /api/auth/create-account
  *
  * --------------- */
 
 export const authApiSlice = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({})
+  endpoints: (builder) => ({
+    createAccount: builder.mutation({
+      query: (data) => ({
+        url: '/api/auth/create-account',
+        method: 'POST',
+        body: data,
+      }),
+    })
+  })
 })
+
+export const {
+  useCreateAccountMutation
+} = authApiSlice
