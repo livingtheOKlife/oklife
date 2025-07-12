00844d9153ab398b3fbe3ebeea560fecbb6550fd
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-07-12 09:27:02 +0100

    fix[client]: add self centering to input error components

--- client/src/components/shared/forms/InputError.tsx
+++ client/src/components/shared/forms/InputError.tsx
@@ -8,7 +8,8 @@ const InputError = ({error}:Type) => {
   const { palette } = useTheme()
   return (
     <span style={{
-      margin: '-0.125rem 0 0.5rem 0.5rem',
+      alignSelf: 'center',
+      width: 'calc(100% - 1rem)',
       fontSize: '8px',
       fontWeight: 500,
       color: palette.wheel.red.main
