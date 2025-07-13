1578b62d32f2a2ac2acf5a7e601d0384f95c3172
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-07-13 23:46:07 +0100

    feat[client]: add verify account link to menu navigation

--- client/src/components/MenuNav.tsx
+++ client/src/components/MenuNav.tsx
@@ -1,11 +1,22 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useSignOutMutation } from '../api/authApiSlice'
 import FlexBetween from './shared/utils/FlexBetween'
 import { useNavigate } from 'react-router-dom'
 import { useAppContext } from '../hooks/useAppContext'
 import { clearCredentials } from '../api/authSlice'
-import { Button, useMediaQuery, useTheme } from '@mui/material'
+import { Box, Button, styled, useMediaQuery, useTheme } from '@mui/material'
 import Loading from './shared/Loading'
+import type { AuthStateType } from '../store'
+
+const MenuNavItem = styled(Box)(({ theme }) => ({
+  padding: '0.75rem',
+  border: `1px solid ${theme.palette.grey[700]}`,
+  borderRadius: '4px',
+  textAlign: 'center',
+  lineHeight: 1,
+  cursor: 'pointer',
+  transition: 'all 0.08s ease-in-out'
+}))
 
 /**------------------------------ menu navigation panel
  * 
@@ -19,6 +30,8 @@ import Loading from './shared/Loading'
 const MenuNav = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const { userInfo } = useSelector((state:AuthStateType) => state.auth)
+  const user = userInfo.user
   const { palette } = useTheme()
   const darkMode = useMediaQuery('(prefers-color-scheme: dark')
   const { setMenuInactive, setAlertActive } = useAppContext()
@@ -38,7 +51,18 @@ const MenuNav = () => {
   return (
     <FlexBetween component='nav' id='menu-nav' flexDirection='column' height='100%'>
       <ul style={{ height: '100%', width: '100%' }}>
-        <li>MenuContainer - Menu Nav Item</li>
+        {
+          !user.isVerified && <MenuNavItem sx={{
+            '&:hover': {
+              border: `1px solid ${palette.primary.main}`,
+              backgroundColor: palette.primary.main,
+              color: darkMode ? palette.background.paper : palette.background.default,
+            }
+          }} onClick={() => {
+            navigate('/verify-account')
+            setMenuInactive()
+          }}>Verify Account</MenuNavItem>
+        }
       </ul>
       {
         isLoading ? <Loading type='' />
