2aa80466a6b75de7924fb06ee2d9e81091ba40e0
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-07-14 21:01:36 +0100

    feat[client]: add form to forgotten password page

--- client/src/pages/ForgotPasswordPage.tsx
+++ client/src/pages/ForgotPasswordPage.tsx
@@ -1,4 +1,11 @@
+import { Divider, FormControl, FormLabel, useTheme } from '@mui/material'
 import MainContainer from '../components/layout/MainContainer'
+import FormHeader from '../components/shared/forms/FormHeader'
+import FormWidget from '../components/shared/forms/FormWidget'
+import FormButton from '../components/shared/forms/FormButton'
+import { TbMailFilled } from 'react-icons/tb'
+import FormFooter from '../components/shared/forms/FormFooter'
+import { Link } from 'react-router-dom'
 
 /**------------------------------ forgot password page
  *
@@ -10,9 +17,29 @@ import MainContainer from '../components/layout/MainContainer'
  * --------------- */
 
 const ForgotPasswordPage = () => {
+  const { palette } = useTheme()
+  const onSubmit = () => {}
   return (
     <MainContainer page='forgot-password-page'>
-      MainContainer - Forgot Password Page
+      <FormWidget onSubmit={onSubmit}>
+        <FormHeader>
+          <h2>Don't Panic!</h2>
+          <span style={{ fontSize: '12px' }}>Enter your email below, we will take it from there...</span>
+        </FormHeader>
+        <FormControl>
+          <FormLabel style={{ marginLeft: '0.5rem', color: palette.text.primary }}>Email</FormLabel>
+          <input type="email" placeholder='Enter your email address' />
+        </FormControl>
+        <FormButton>
+          <TbMailFilled />
+          Send help
+        </FormButton>
+        <Divider>or</Divider>
+        <FormFooter>
+          <span>You remember now?</span>
+          <Link to='/sign-in' style={{ color: palette.primary.main }}>Sign in instead</Link>
+        </FormFooter>
+      </FormWidget>
     </MainContainer>
   )
 }
