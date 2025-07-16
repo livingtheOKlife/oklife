36313c3f5524031a2175101e5789d7b82b37d359
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-07-16 21:50:46 +0100

    feat[client]: add authentication route - initial update password page

---
+++ client/src/pages/UpdatePasswordPage.tsx
@@ -0,0 +1,20 @@
+import MainContainer from '../components/layout/MainContainer'
+
+/**------------------------------ update password page
+ *
+ * @name UpdatePasswordPage
+ * @function
+ * @route /update-password
+ * @returns main container component for the update password page
+ *
+ * --------------- */
+
+const UpdatePasswordPage = () => {
+  return (
+    <MainContainer page='update-password-page'>
+      MainContainer - Update Password Page
+    </MainContainer>
+  )
+}
+
+export default UpdatePasswordPage
